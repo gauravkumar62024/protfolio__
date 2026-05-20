07832ec2d737f7d4f099d906618922a2eb3afc2e
GAURAV KUMAR <78220135+gauravkumar62024@users.noreply.github.com>
2026-05-20 11:34:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: isGithubPages ? "/protfolio__" : "",
-  assetPrefix: isGithubPages ? "/protfolio__/" : "",
 };
 
 export default nextConfig;
